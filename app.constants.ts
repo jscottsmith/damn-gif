ffbc83b81f923cdd3a891126f05a39aa1108b087
J Scott Smith <jscsmith@gmail.com>
2022-07-10 22:57:35 -0700

    feat: cycle through gallery images

--- app.constants.ts
+++ app.constants.ts
@@ -2,4 +2,14 @@ export const GIPHY_API_URL = "https://api.giphy.com/v1";
 export const GIPHY_BY_ID = "/gifs";
 
 // NOTE: Collection of ids to fetch from Giphy since there's no endpoint to fetch all images by user.
-export const DAMN_GIPHY_IDS = ["deDPZIj4k6TEDz93Ox", "vfq2ingvpCMIxkys0G"];
+export const DAMN_GIPHY_IDS = [
+  "brEPm9jJGQ5SzoNxTQ",
+  "wrnWSsQf1Uy1l8DNut",
+  "QUbhawNgHOF1xpuaJc",
+  "SecYvlyS0YDmeuaMDd",
+  "deDPZIj4k6TEDz93Ox",
+  "vfq2ingvpCMIxkys0G",
+  "LWkomgHVsl4EtxsWm1",
+  "ppoPEBgPlc7GOwPr44",
+  "yNsKjmZqGbUnpjJnKL",
+];
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,14 @@
 import { useGalleryContext } from "../components/gallery-context-provider/index";
+import { useCycleIndex } from "../hooks/use-cycle-index";
 
 export function HomeRoute() {
   const images = useGalleryContext();
+  const indexController = useCycleIndex(images.length);
+  const currentImage = images[indexController.index];
   return (
-    <>
+    <div onClick={() => indexController.next()}>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {images.map((item) => (
-        <img src={item.images.original.url} key={item.id} />
-      ))}
-    </>
+      <img src={currentImage.images.original.url} />
+    </div>
   );
 }
