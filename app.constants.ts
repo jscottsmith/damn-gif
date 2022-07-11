465f3b5eb9f8d17964be4747348e02e2eead5f83
J Scott Smith <jscsmith@gmail.com>
2022-07-10 22:02:28 -0700

    chore: add fetch fn for giphy images

--- app.constants.ts
+++ app.constants.ts
@@ -1,2 +1,5 @@
 export const GIPHY_API_URL = "https://api.giphy.com/v1";
 export const GIPHY_BY_ID = "/gifs";
+
+// NOTE: Collection of ids to fetch from Giphy since there's no endpoint to fetch all images by user.
+export const DAMN_GIPHY_IDS = ["deDPZIj4k6TEDz93Ox", "vfq2ingvpCMIxkys0G"];
