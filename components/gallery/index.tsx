1982b643055887c1a6dfd99744ce8f6e7a802137
J Scott Smith <jscsmith@gmail.com>
2022-07-11 23:04:59 -0700

    feat: animate presence of images

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { useGalleryContext } from "../../components/gallery-context-provider/index";
 import { useCycleIndex } from "../../hooks/use-cycle-index";
 import { Image } from "./components/image";
@@ -11,17 +12,33 @@ export function Gallery() {
   return (
     <section className="select-none">
       <div className="flex h-screen items-center justify-center overflow-hidden p-2">
-        <DraggableSlide
-          key={currentImage.id}
-          onThrowLeftComplete={() => indexController.next()}
-          onThrowRightComplete={() => indexController.next()}
-        >
-          <Image
-            src={currentImage.images.original.url}
-            smallImageSrc={currentImage.images.fixed_width_small_still.url}
-            draggable={false}
-          />
-        </DraggableSlide>
+        <AnimatePresence>
+          <motion.div
+            key={currentImage.id}
+            initial={{ scale: 0 }}
+            animate={{
+              scale: 1,
+              transition: {
+                delay: 0.5,
+                type: "spring",
+                damping: 50,
+                stiffness: 200,
+              },
+            }}
+            exit={{ opacity: 0, scale: 1.25, position: "absolute" }}
+          >
+            <DraggableSlide
+              onThrowLeftComplete={() => indexController.next()}
+              onThrowRightComplete={() => indexController.next()}
+            >
+              <Image
+                src={currentImage.images.original.url}
+                smallImageSrc={currentImage.images.fixed_width_small_still.url}
+                draggable={false}
+              />
+            </DraggableSlide>
+          </motion.div>
+        </AnimatePresence>
       </div>
       <nav className="fixed bottom-0 left-0 flex w-full">
         <button
