655265ee33bab649371b30058f4a06236dc24772
J Scott Smith <jscsmith@gmail.com>
2022-07-11 18:22:01 -0700

    feat: add draggable slides with framer motion

---
+++ components/gallery/components/draggable-slide.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { motion, useMotionValue, useSpring, useAnimation } from "framer-motion";
+
+type Direction = "left" | "right";
+
+export function DraggableSlide(
+  props: React.PropsWithChildren<{
+    onThrowLeftComplete?: () => unknown;
+    onThrowRightComplete?: () => unknown;
+  }>
+) {
+  const constraintsRef = React.useRef<HTMLDivElement>();
+  const [constrained, setConstrained] = React.useState(true);
+  const [direction, setDirection] = React.useState<Direction>();
+
+  // NOTE: this is used once the drag completes and a throw is triggered
+  // the controls are use to finish the animation off the screen
+  const controls = useAnimation();
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const xSmooth = useSpring(x, { damping: 10, stiffness: 100 });
+
+  const throwEnd = (min: number) => {
+    const direction = xSmooth.get() < 0 ? "left" : "right";
+    const velocity = xSmooth.getVelocity();
+
+    setDirection(direction);
+
+    // NOTE: gets the distance to throw if we meet the min threshold
+    const throwDistance = () => {
+      const containerWidth = window.innerWidth;
+      const childWidth = constraintsRef.current
+        ? constraintsRef.current.getBoundingClientRect().width
+        : 0;
+
+      return direction === "left"
+        ? -containerWidth / 2 - childWidth
+        : containerWidth / 2 + childWidth;
+    };
+
+    if (Math.abs(velocity) > min) {
+      setConstrained(false);
+      controls.start({
+        x: throwDistance(),
+        y: y.get(),
+        transition: { type: "spring", stiffness: 100 },
+        // transition: {
+        //   type: "inertia",
+        // },
+      });
+    }
+  };
+
+  const onAnimationComplete = () => {
+    if (direction === "left") {
+      props.onThrowLeftComplete && props.onThrowLeftComplete();
+    }
+    if (direction === "right") {
+      props.onThrowRightComplete && props.onThrowRightComplete();
+    }
+  };
+
+  return (
+    <motion.div
+      // @ts-expect-error
+      ref={constraintsRef}
+      className="cursor-grab"
+      drag
+      dragElastic={2}
+      dragConstraints={constrained && { left: 0, right: 0, top: 0, bottom: 0 }}
+      animate={controls}
+      whileDrag={{
+        backgroundColor: "#000",
+      }}
+      style={{ x, y }}
+      onDragEnd={() => throwEnd(200)}
+      onAnimationComplete={onAnimationComplete}
+    >
+      {props.children}
+    </motion.div>
+  );
+}
--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,14 +1,21 @@
 import { useGalleryContext } from "../../components/gallery-context-provider/index";
 import { useCycleIndex } from "../../hooks/use-cycle-index";
+import { DraggableSlide } from "./components/draggable-slide";
 
 export function Gallery() {
   const images = useGalleryContext();
   const indexController = useCycleIndex(images.length);
   const currentImage = images[indexController.index];
   return (
-    <section>
-      <div className="flex h-screen items-center justify-center">
-        <img src={currentImage.images.original.url} />
+    <section className="select-none">
+      <div className="flex h-screen items-center justify-center overflow-hidden p-2">
+        <DraggableSlide
+          key={currentImage.id}
+          onThrowLeftComplete={() => indexController.next()}
+          onThrowRightComplete={() => indexController.next()}
+        >
+          <img src={currentImage.images.original.url} draggable={false} />
+        </DraggableSlide>
       </div>
       <nav className="fixed bottom-0 left-0 flex w-full">
         <button
