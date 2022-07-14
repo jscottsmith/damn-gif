c19c0bbe6ab3daff8cee9e506d22cd67698a0354
J Scott Smith <jscsmith@gmail.com>
2022-07-13 22:02:39 -0700

    chore: update copy

--- components/page-loader.tsx
+++ components/page-loader.tsx
@@ -6,14 +6,16 @@ export function PageLoader() {
     <div className="fixed inset-0 flex items-center justify-center bg-slate-100">
       <div>
         <motion.div
-          className="mb-4 origin-center"
+          className="mb-4 flex justify-center"
           initial={{ scale: 0.5 }}
-          animate={{ scale: [1, 0.5, 1] }}
+          animate={{ scale: [0.7, 0.5, 0.7] }}
           transition={{ repeat: Infinity, duration: 2 }}
         >
           <LogoEye />
         </motion.div>
-        <p className="text-center text-sm text-gray-400">Loading</p>
+        <p className="text-center text-sm text-gray-400">
+          Loading some things, hang tight.
+        </p>
       </div>
     </div>
   );
